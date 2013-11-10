041912bf2ffb57cb2fca70eb8a6c46913e2be8cd
Bishal Ghimire <bghimire@braindigit.com>
2013-11-10 07:50:46 +0545

    Initial Commit

---
+++ BGCategory/BGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BGAppDelegate.h
+//  BGCategory
+//
+//  Created by Bishal Ghimire on 11/10/13.
+//  Copyright (c) 2013 Bishal Ghimire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BGCategory/BGViewController.h
@@ -0,0 +1,13 @@
+//
+//  BGViewController.h
+//  BGCategory
+//
+//  Created by Bishal Ghimire on 11/10/13.
+//  Copyright (c) 2013 Bishal Ghimire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BGViewController : UIViewController
+
+@end
